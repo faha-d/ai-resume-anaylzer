8af256c6fa5d1c8d6cc81f008b33d1190320da47
fahad <fahad.zahid2004@gmail.com>
2026-06-13 01:24:19 +0500

    packages installed public assets added

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,20 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Untitled Project" },
+    { name: "description", content: "Untitled Router!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className='bg-[url'>
+  <section className="main-section">
+    <div className="page-heading">
+      <h1>Track your applications & Resume</h1>
+      <h2>Review your submissions and check AI-powered feedback.</h2>
+    </div>
+  </section>
+  </main>
 }
