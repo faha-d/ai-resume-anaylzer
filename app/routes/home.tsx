2d24cd17e925cc6eea07a18ff8e38430512ce433
fahad <fahad.zahid2004@gmail.com>
2026-06-13 01:47:10 +0500

    Navbar added.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,20 +1,22 @@
-import type { Route } from "./+types/home";
+import type {Route} from "./+types/home";
+import Navbar from "~/components/Navbar";
 
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "Untitled Project" },
-    { name: "description", content: "Untitled Router!" },
-  ];
+    return [
+        {title: "Untitled Project"},
+        {name: "description", content: "Untitled Router!"},
+    ];
 }
 
 export default function Home() {
-  return <main className='bg-[url'>
-  <section className="main-section">
-    <div className="page-heading">
-      <h1>Track your applications & Resume</h1>
-      <h2>Review your submissions and check AI-powered feedback.</h2>
-    </div>
-  </section>
-  </main>
+    return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+        <Navbar/>
+        <section className="main-section">
+            <div className="page-heading">
+                <h1>Track your applications & Resume</h1>
+                <h2>Review your submissions and check AI-powered feedback.</h2>
+            </div>
+        </section>
+    </main>
 }
