a6dacafa54967dd95be679a93c15b4803fede804
fahad <fahad.zahid2004@gmail.com>
2026-06-14 02:49:59 +0500

    resume card, navbar design completed.

---
+++ app/components/ResumeCard.tsx
@@ -0,0 +1,30 @@
+import {Link} from "react-router"
+import {resume} from "react-dom/server";
+import ScoreCircle from "~/components/ScoreCircle";
+
+const ResumeCard = ({resume: {id, companyName, jobTitle, feedback, imagePath}}: { resume: Resume }) => {
+    return (
+        <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
+            <div className="resume-card-header">
+                <div className="flex flex-col gap-2">
+                    <h2 className="text-black! font-bold wrap-break-word">{companyName}</h2>
+                    <h3 className="text-lg wrap-break-word text-gray-500">
+                        {jobTitle}
+                    </h3>
+                </div>
+
+                <div className="shrink-0">
+                    <ScoreCircle score={feedback.overallScore}/>
+                </div>
+            </div>
+
+            <div className="gradient-border animate-in fade-in duration-1000">
+                <div className="w-full h-full">
+                    <img src={imagePath} alt="Resume" className="w-full -object-cover h-[350px] max-sm:h-[200px0]"/>
+                </div>
+            </div>
+        </Link>
+    )
+}
+
+export default ResumeCard
